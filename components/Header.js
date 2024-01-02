19eba067c01786ff646438d91fae0d02834cb005
spasovags <eexemplega@gmail.com>
2024-01-02 15:34:17 +0200

    overflow hid

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,11 @@ import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
-    <h1 className={`${styles["header-container"]}`}>W   e   l   c   o   m   e   t   o   m   y   b   l   o   g   !</h1>    
+    <div className={`${styles["overf"]}`}>
+      <h1 className={`${styles["header-container"]}`}>W   e   l   c   o   m   e   t   o   m   y   b   l   o   g   !</h1>    
+    </div>
   )
+
 }
 
 export default Header
